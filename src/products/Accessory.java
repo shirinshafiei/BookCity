b2ab189256a11748f6a20183fff8e8173e3bdbe7
Shirin Shafiei <shirin84shafiei@gmail.com>
2025-04-29 22:00:34 +0330

    craet products

--- src/products/Accessory.java
+++ src/products/Accessory.java
@@ -1,4 +1,22 @@
 package products;
 
-public class Accessory {
+public class Accessory extends Product{
+    private String color;
+    private static int accessoryCounter = 0;
+
+    public Accessory(String title, Double price, String color){
+        super(title,price);
+        this.color = color;
+    }
+
+    @Override
+    protected String generateId() {
+        accessoryCounter++;
+        return "3" + String.format("%03d", accessoryCounter);
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " color: " + color;
+    }
 }
--- src/products/Book.java
+++ src/products/Book.java
@@ -1,4 +1,26 @@
 package products;
 
-public class Book {
+public class Book extends Product {
+    private String author;
+    private String publication;
+    private String genre;
+    private static int bookCounter = 0;
+
+    public Book(String title, Double price, String author, String publication, String genre) {
+        super(title,price);
+        this.author = author;
+        this.publication = publication;
+        this.genre = genre;
+
+    }
+    @Override
+    public String toString(){
+        return super.toString() + " author: " + author + " publication: " + publication + " genre: " + genre;
+    }
+
+    @Override
+    protected String generateId() {
+        bookCounter++;
+        return "1" + String.format("%03d", bookCounter);
+    }
 }
